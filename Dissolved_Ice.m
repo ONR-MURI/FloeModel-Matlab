2e9f902a2e51bea6bde04989907444a7dba2df59
bpm5026 <33733291+bpm5026@users.noreply.github.com>
2020-02-13 12:14:54 -0800

    Update Dissolved_Ice.m
    Updated to include ghost cells

--- Dissolved_Ice.m
+++ Dissolved_Ice.m
@@ -1,28 +1,69 @@
 function Vd_new = Dissolved_Ice(Vd,coarseMean,im_num,dissolved_new,c2_boundary,dt)
-%% find current and old terms
-u = squeeze(coarseMean(2,:,:,im_num));
-v = squeeze(coarseMean(3,:,:,im_num));
-Vdcurrent = Vd(:,:,1);
-[Ny,Nx] = size(u);
+%% find current terms
+U = squeeze(coarseMean(2,:,:,im_num));
+V = squeeze(coarseMean(3,:,:,im_num));
+vdcurrent = Vd(:,:,1);
+
+%Add ghost cells for current time step
+[Ny,Nx] = size(U);
+Nx = Nx+2;
+Ny = Ny+2;
+u(2:Ny-1,2:Nx-1) = U;
+u(1,:) = u(2,:);
+u(Ny,:) = u(Ny-1,:);
+u(:,1) = u(:,2);
+u(:,Nx) = u(:,Nx-1);
+v(2:Ny-1,2:Nx-1) = V;
+v(1,:) = v(2,:);
+v(Ny,:) = v(Ny-1,:);
+v(:,1) = v(:,2);
+v(:,Nx) = v(:,Nx-1);
+Vdcurrent(2:Ny-1,2:Nx-1) = vdcurrent;
+Vdcurrent(1,:) = Vdcurrent(2,:);
+Vdcurrent(Ny,:) = Vdcurrent(Ny-1,:);
+Vdcurrent(:,1) = Vdcurrent(:,2);
+Vdcurrent(:,Nx) = Vdcurrent(:,Nx-1);
+
+%Set ghost cells for old terms
 if im_num==1
     uold = u;
     vold = v;
     Vdold = Vdcurrent;
 else
-    uold = squeeze(coarseMean(2,:,:,im_num-1));
-    vold = squeeze(coarseMean(3,:,:,im_num-1));
-    Vdold = Vd(:,:,2);
+    Uold = squeeze(coarseMean(2,:,:,im_num-1));
+    Vold = squeeze(coarseMean(3,:,:,im_num-1));
+    vdold = Vd(:,:,2);
 end
+uold(2:Ny-1,2:Nx-1) = Uold;
+uold(1,:) = uold(2,:);
+uold(Ny,:) = uold(Ny-1,:);
+uold(:,1) = uold(:,2);
+uold(:,Nx) = uold(:,Nx-1);
+vold(2:Ny-1,2:Nx-1) = Vold;
+vold(1,:) = vold(2,:);
+vold(Ny,:) = vold(Ny-1,:);
+vold(:,1) = vold(:,2);
+vold(:,Nx) = vold(:,Nx-1);
+Vdold(2:Ny-1,2:Nx-1) = vdold;
+Vdold(1,:) = Vdold(2,:);
+Vdold(Ny,:) = Vdold(Ny-1,:);
+Vdold(:,1) = Vdold(:,2);
+Vdold(:,Nx) = Vdold(:,Nx-1);
+Dissolved_new = zeros(Ny,Nx);
+Dissolved_new(2:Ny-1,2:Nx-1) = dissolved_new;
 
 %define strength of diffusion
-diffusion = 0.5;
+diffusion = 1e4;
 
 %% Define grid
-x = min(c2_boundary(1,:)):(max(c2_boundary(1,:))-min(c2_boundary(1,:)))/Nx:max(c2_boundary(1,:));
-y = min(c2_boundary(2,:)):(max(c2_boundary(2,:))-min(c2_boundary(2,:)))/Ny:max(c2_boundary(2,:));
+x = min(c2_boundary(1,:)):(max(c2_boundary(1,:))-min(c2_boundary(1,:)))/(Nx-2):max(c2_boundary(1,:));
+y = min(c2_boundary(2,:)):(max(c2_boundary(2,:))-min(c2_boundary(2,:)))/(Ny-2):max(c2_boundary(2,:));
+delx = abs(x(2)-x(1));
+dely = abs(y(2)-y(1));
+x = [min(x)-delx x max(x)+delx];
+y = [min(y)-dely y max(y)+dely];
 
 %% Find differentiation matrix for x
-delx = abs(x(2)-x(1));
 dx = zeros(Nx,Nx);
 a = 1/(2*delx);
 dx(Nx+1:1+Nx:Nx*Nx) = a;
@@ -34,7 +75,6 @@ dx(Nx,Nx) = 2*a;
 d2x = dx^2;
 
 %Find differentiation matrix for y
-dely = abs(y(2)-y(1));
 dy = zeros(Ny,Ny);
 b = 1/(2*dely);
 dy(Ny+1:1+Ny:Ny*Ny) = b;
@@ -69,22 +109,28 @@ for ii = 1:Ny
     L(Nx*Ny-Ny+ii,Nx*Ny-2*Ny + ii) = -2*a;
     L(Nx*Ny-Ny+ii,Nx*Ny-Ny+ii) = 2*a;
 end
-
+%t = 0;
 %% Time step new equation for dissolved ice. Adams Basheforth for NL terms and Crank-Nicolsen for linear terms
-RHS = Vdcurrent(:)+dt*(dissolved_new(:) + diffusion/2*(kron(Ix,d2y)+kron(d2x,Iy))*Vdcurrent(:)...
-    -1/2*(3*Dx*(u(:).*Vdcurrent(:))-Dx*(uold(:).*Vdold(:))) - 1/2*(3*Dy*(v(:).*Vdcurrent(:))-Dy*(vold(:).*Vdold(:))));
-
-%Setting RHS to 0 for BCs
-RHS(1:Ny:Nx*Ny-1) = 0;
-RHS(Ny:Ny:Nx*Ny) = 0;
-RHS(1:Ny) = 0;
-RHS(Nx*Ny-Ny:Nx*Ny) = 0;
-
-%Invert, reshape and take care of corners
-VdNEW = L\RHS;
-Vd_new = reshape(VdNEW,Ny,Nx);
-Vd_new(1,1) = mean([Vd_new(1,2),Vd_new(2,1)]);
-Vd_new(1,Nx) = mean([Vd_new(1,Nx-1),Vd_new(2,Nx)]);
-Vd_new(Ny,1) = mean([Vd_new(Ny,2),Vd_new(Ny-1,1)]);
-Vd_new(Ny,Nx) = mean([Vd_new(Ny,Nx-1),Vd_new(Ny-1,Nx)]);
+%while t < tnew
+    RHS = Vdcurrent(:)+Dissolved_new(:)+dt*(diffusion/2*(kron(Ix,d2y)+kron(d2x,Iy))*Vdcurrent(:)...
+        -1/2*(3*u(:).*(Dx*Vdcurrent(:))-uold(:).*(Dx*Vdold(:))) - 1/2*(3*v(:).*(Dy*Vdcurrent(:))-vold(:).*(Dy*Vdold(:))));
+    
+    %Setting RHS to 0 for BCs
+    RHS(1:Ny:Nx*Ny-1) = 0;
+    RHS(Ny:Ny:Nx*Ny) = 0;
+    RHS(1:Ny) = 0;
+    RHS(Nx*Ny-Ny:Nx*Ny) = 0;
+    
+    %Invert, reshape and take care of corners
+    VdNEW = L\RHS;
+    Vd_new = reshape(VdNEW,Ny,Nx);
+    Vd_new(1,1) = mean([Vd_new(1,2),Vd_new(2,1)]);
+    Vd_new(1,Nx) = mean([Vd_new(1,Nx-1),Vd_new(2,Nx)]);
+    Vd_new(Ny,1) = mean([Vd_new(Ny,2),Vd_new(Ny-1,1)]);
+    Vd_new(Ny,Nx) = mean([Vd_new(Ny,Nx-1),Vd_new(Ny-1,Nx)]);
+%    Vdold = Vdcurrent;
+%    Vdcurrent = Vd_new;
+%    t = t+dt;
+%end
+Vd_new = Vd_new(2:Ny-1,2:Nx-1);
 end
