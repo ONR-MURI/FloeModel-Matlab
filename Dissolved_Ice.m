cdec27f3e8fcac003c8457bce28ddc123fb799c7
bpm5026 <33733291+bpm5026@users.noreply.github.com>
2020-03-02 14:34:05 -0800

    Ridging update

--- Dissolved_Ice.m
+++ Dissolved_Ice.m
@@ -6,131 +6,84 @@ vdcurrent = Vd(:,:,1);
 
 %Add ghost cells for current time step
 [Ny,Nx] = size(U);
-Nx = Nx+2;
-Ny = Ny+2;
-u(2:Ny-1,2:Nx-1) = U;
-u(1,:) = u(2,:);
-u(Ny,:) = u(Ny-1,:);
-u(:,1) = u(:,2);
-u(:,Nx) = u(:,Nx-1);
-v(2:Ny-1,2:Nx-1) = V;
-v(1,:) = v(2,:);
-v(Ny,:) = v(Ny-1,:);
-v(:,1) = v(:,2);
-v(:,Nx) = v(:,Nx-1);
-Vdcurrent(2:Ny-1,2:Nx-1) = vdcurrent;
-Vdcurrent(1,:) = Vdcurrent(2,:);
-Vdcurrent(Ny,:) = Vdcurrent(Ny-1,:);
-Vdcurrent(:,1) = Vdcurrent(:,2);
-Vdcurrent(:,Nx) = Vdcurrent(:,Nx-1);
+Nx = Nx+4;
+Ny = Ny+4;
+u(3:Ny-2,3:Nx-2) = U;
+u(1:2,:) = [u(3,:); u(3,:)];
+u(Ny-1:Ny,:) = [u(Ny-2,:);u(Ny-2,:)];
+u(:,1:2) = [u(:,3) u(:,3)];
+u(:,Nx-1:Nx) = [u(:,Nx-2) u(:,Nx-2)];
+v(3:Ny-2,3:Nx-2) = V;
+v(1:2,:) = [v(3,:); v(3,:)];
+v(Ny-1:Ny,:) = [v(Ny-2,:);v(Ny-2,:)];
+v(:,1:2) = [v(:,3) v(:,3)];
+v(:,Nx-1:Nx) = [v(:,Nx-2) v(:,Nx-2)];
+Vdcurrent(3:Ny-2,3:Nx-2) = vdcurrent;
+Vdcurrent(1:2,:) = [Vdcurrent(3,:); Vdcurrent(3,:)];
+Vdcurrent(Ny-1:Ny,:) = [Vdcurrent(Ny-2,:);Vdcurrent(Ny-2,:)];
+Vdcurrent(:,1:2) = [Vdcurrent(:,3) Vdcurrent(:,3)];
+Vdcurrent(:,Nx-1:Nx) = [Vdcurrent(:,Nx-2) Vdcurrent(:,Nx-2)];
 
-%Set ghost cells for old terms
-if im_num==1
-    uold = u;
-    vold = v;
-    Vdold = Vdcurrent;
-else
-    Uold = squeeze(coarseMean(2,:,:,im_num-1));
-    Vold = squeeze(coarseMean(3,:,:,im_num-1));
-    vdold = Vd(:,:,2);
-end
-uold(2:Ny-1,2:Nx-1) = Uold;
-uold(1,:) = uold(2,:);
-uold(Ny,:) = uold(Ny-1,:);
-uold(:,1) = uold(:,2);
-uold(:,Nx) = uold(:,Nx-1);
-vold(2:Ny-1,2:Nx-1) = Vold;
-vold(1,:) = vold(2,:);
-vold(Ny,:) = vold(Ny-1,:);
-vold(:,1) = vold(:,2);
-vold(:,Nx) = vold(:,Nx-1);
-Vdold(2:Ny-1,2:Nx-1) = vdold;
-Vdold(1,:) = Vdold(2,:);
-Vdold(Ny,:) = Vdold(Ny-1,:);
-Vdold(:,1) = Vdold(:,2);
-Vdold(:,Nx) = Vdold(:,Nx-1);
-Dissolved_new = zeros(Ny,Nx);
-Dissolved_new(2:Ny-1,2:Nx-1) = dissolved_new;
 
 %define strength of diffusion
 diffusion = 1e4;
+
+Dissolved_new = zeros(Ny,Nx);
+Dissolved_new(3:Ny-2,3:Nx-2) = dissolved_new;
 
 %% Define grid
 x = min(c2_boundary(1,:)):(max(c2_boundary(1,:))-min(c2_boundary(1,:)))/(Nx-2):max(c2_boundary(1,:));
 y = min(c2_boundary(2,:)):(max(c2_boundary(2,:))-min(c2_boundary(2,:)))/(Ny-2):max(c2_boundary(2,:));
 delx = abs(x(2)-x(1));
-dely = abs(y(2)-y(1));
-x = [min(x)-delx x max(x)+delx];
-y = [min(y)-dely y max(y)+dely];
+dely = -abs(y(2)-y(1));
 
 %% Find differentiation matrix for x
-dx = zeros(Nx,Nx);
-a = 1/(2*delx);
-dx(Nx+1:1+Nx:Nx*Nx) = a;
-dx(2:1+Nx:Nx*Nx-Nx) = -a;
-dx(1,2) = 2*a;
-dx(1,1) = -2*a;
-dx(Nx,Nx-1) = -2*a;
-dx(Nx,Nx) = 2*a;
-d2x = dx^2;
+d2x = zeros(Nx,Nx);
+b = 1/(delx^2);
+d2x(2:Nx+1:Nx*Nx) = b;
+d2x(2+Nx:Nx+1:Nx*Nx) = -2*b;
+d2x(2+2*Nx:Nx+1:Nx*Nx) = b;
+d2x(1,1) = b/(3/2);
+d2x(1,2) = -2*b/(3/2);
+d2x(1,3) = b/(3/2);
+d2x(Nx,Nx) = b/(3/2);
+d2x(Nx,Nx-1) = -2*b/(3/2);
+d2x(Nx,Nx-2) = b/(3/2);
 
 %Find differentiation matrix for y
-dy = zeros(Ny,Ny);
-b = 1/(2*dely);
-dy(Ny+1:1+Ny:Ny*Ny) = b;
-dy(2:1+Ny:Ny*Ny-Ny) = -b;
-dy(1,2) = 2*b;
-dy(1,1) = -2*b;
-dy(Ny,Ny-1) = -2*b;
-dy(Ny,Ny) = 2*b;
-d2y = dy^2;
+d2y = zeros(Ny,Ny);
+b = 1/(dely^2);
+d2y(2:Ny+1:Ny*Ny) = b;
+d2y(2+Ny:Ny+1:Ny*Ny) = -2*b;
+d2y(2+2*Ny:Ny+1:Ny*Ny) = b;
+d2y(1,1) = b/(3/2);
+d2y(1,2) = -2*b/(3/2);
+d2y(1,3) = b/(3/2);
+d2y(Ny,Ny) = b/(3/2);
+d2y(Ny,Ny-1) = -2*b/(3/2);
+d2y(Ny,Ny-2) = b/(3/2);
 
-%% Use Kroenicker product to allow for differentiation in both x and y direction
 Ix = eye(Nx);
 Iy = eye(Ny);
-Dy = kron(Ix,dy);
-Dx = kron(dx,Iy);
-L = kron(Iy,Ix)-diffusion*dt/2*(kron(Ix,d2y)+kron(d2x,Iy));
 
-%Add Boundary conditions
-%Dy = 0 at top and bottom 
-for ii = Ny:Ny:Nx*Ny
-    L(ii,ii-1) = -2*a;
-    L(ii,ii) = 2*a;
-end
-for ii = 0:Ny:Nx*Ny-Ny
-    L(ii+1,ii+1) = -2*a;
-    L(ii+1,ii+2) = 2*a;
-end
-%Dx = 0 at left and right
-for ii = 1:Ny
-    L(ii,ii) = -2*a;
-    L(ii,ii+Ny) = 2*a;
-    L(Nx*Ny-Ny+ii,Nx*Ny-2*Ny + ii) = -2*a;
-    L(Nx*Ny-Ny+ii,Nx*Ny-Ny+ii) = 2*a;
-end
-%t = 0;
+%% Shift points for calculating Advective Terms
+Vxshift = zeros(Ny-4,Nx-3);
+Vxshift(:,2:end-1) = 0.5*(Vdcurrent(3:Ny-2,3:Nx-3)+Vdcurrent(3:Ny-2,4:Nx-2));
+Ushift = zeros(Ny-4,Nx-3);
+Ushift(:,2:end-1) = 0.5*(u(3:Ny-2,3:Nx-3)+u(3:Ny-2,4:Nx-2));
+Vyshift = zeros(Ny-3,Nx-4);
+Vyshift(2:end-1,:) = 0.5*(Vdcurrent(3:Ny-3,3:Nx-2)+Vdcurrent(4:Ny-2,3:Nx-2));
+Vshift = zeros(Ny-3,Nx-4);
+Vshift(2:end-1,:) = 0.5*(v(3:Ny-3,3:Nx-2)+v(4:Ny-2,3:Nx-2));
 %% Time step new equation for dissolved ice. Adams Basheforth for NL terms and Crank-Nicolsen for linear terms
-%while t < tnew
-    RHS = Vdcurrent(:)+Dissolved_new(:)+dt*(diffusion/2*(kron(Ix,d2y)+kron(d2x,Iy))*Vdcurrent(:)...
-        -1/2*(3*u(:).*(Dx*Vdcurrent(:))-uold(:).*(Dx*Vdold(:))) - 1/2*(3*v(:).*(Dy*Vdcurrent(:))-vold(:).*(Dy*Vdold(:))));
-    
-    %Setting RHS to 0 for BCs
-    RHS(1:Ny:Nx*Ny-1) = 0;
-    RHS(Ny:Ny:Nx*Ny) = 0;
-    RHS(1:Ny) = 0;
-    RHS(Nx*Ny-Ny:Nx*Ny) = 0;
-    
-    %Invert, reshape and take care of corners
-    VdNEW = L\RHS;
-    Vd_new = reshape(VdNEW,Ny,Nx);
-    Vd_new(1,1) = mean([Vd_new(1,2),Vd_new(2,1)]);
-    Vd_new(1,Nx) = mean([Vd_new(1,Nx-1),Vd_new(2,Nx)]);
-    Vd_new(Ny,1) = mean([Vd_new(Ny,2),Vd_new(Ny-1,1)]);
-    Vd_new(Ny,Nx) = mean([Vd_new(Ny,Nx-1),Vd_new(Ny-1,Nx)]);
-%    Vdold = Vdcurrent;
-%    Vdcurrent = Vd_new;
-%    t = t+dt;
+%Calculate RHS first without advection terms
+RHS = Vdcurrent(:)+Dissolved_new(:)+dt*diffusion*(kron(Ix,d2y)+kron(d2x,Iy))*Vdcurrent(:);
+Vd_new = reshape(RHS,Ny,Nx);
+
+%calculate advection terms
+Advec = diff(Ushift.*Vxshift,1,2)/delx+ diff(Vshift.*Vyshift,1,1)/dely;
+Vd_new(3:Ny-2,3:Nx-2) = Vd_new(3:Ny-2,3:Nx-2)-dt*Advec;
+
 %end
-Vd_new = Vd_new(2:Ny-1,2:Nx-1);
+Vd_new = Vd_new(3:Ny-2,3:Nx-2);
 end
--- multiple_floes_Fractures.m
+++ multiple_floes_Fractures.m
@@ -34,7 +34,9 @@ ifPlot = true; %Plot floe figures or not?
 
 
 % Calc interactions and plot initial state
-Floe = floe_interactions_all(Floe, ocean, winds,c2_boundary_poly, dt); % find interaction points
+Nx=5; Ny=5;
+dissolvedNEW = zeros(Ny,Nx);
+[Floe, dissolvedNEW] = floe_interactions_all(Floe, ocean, winds,c2_boundary_poly, dt,dissolvedNEW,Nx,Ny); % find interaction points
 Floe=Floe(logical(cat(1,Floe.alive)));
 %plot_Floes_poly(0,0, Floe, ocean, c2_boundary);
 
@@ -42,12 +44,15 @@ Floe=Floe(logical(cat(1,Floe.alive)));
 %ddx=250; % resolution of the original floe images in meters
 %[Xgg, Ygg]=meshgrid(-70e3:ddx:70e3,-70e3:ddx:70e3); % high-res eulerian grid
 %c_fact=40; % coarsening factor
+x = min(c2_boundary(1,:)):(max(c2_boundary(1,:))-min(c2_boundary(1,:)))/Nx:max(c2_boundary(1,:));
+y = min(c2_boundary(2,:)):(max(c2_boundary(2,:))-min(c2_boundary(2,:)))/Ny:max(c2_boundary(2,:));
+x = 0.5*(x(1:end-1)+x(2:end));
+y = 0.5*(y(1:end-1)+y(2:end));
+y = fliplr(y);
 
 %Calc high and low-res Eulerian fields
 %[x,y, cFine0, cCoarse0,  U_Fine0,V_Fine0, U_Coarse0, V_Coarse0 ] = create_eulerian_data( Floe, Xgg, Ygg, c_fact );
-[c,vel,accel] = calc_eulerian_data(Floe,20,20,c2_boundary);
-
-Nx=10; Ny=10;
+[c,vel,accel] = calc_eulerian_data2(Floe,20,20,c2_boundary);
 
 coarseMean=zeros(5,Ny,Nx,nSnapshots);
 coarseSnap=zeros(5,Ny,Nx,nSnapshots);
@@ -126,7 +131,7 @@ while im_num<nSnapshots
     end
     
     %Calculate forces and torques and intergrate forward
-    Floe = floe_interactions_all_periodicBCs(Floe, ocean, winds, c2_boundary_poly, dt);
+    Floe = floe_interactions_all_periodicBCs_bpm(Floe, ocean, winds, c2_boundary_poly, dt,dissolvedNEW,Nx,Ny);
     
     overlapArea=cat(1,Floe.OverlapArea)./cat(1,Floe.area);
     keep=rand(length(Floe),1)>overlapArea;
@@ -139,7 +144,7 @@ while im_num<nSnapshots
     %diluted=length(keep)-sum(keep);
     %if diluted>0, disp(['diluted floes: ' num2str(diluted)]); end
     
-    [c,vel,accel] = calc_eulerian_data(Floe,Nx,Ny,c2_boundary);
+    [c,vel,accel] = calc_eulerian_data2(Floe,Nx,Ny,c2_boundary);
     
     coarseMean(1,:,:,im_num)=squeeze(coarseMean(1,:,:,im_num))+c/nDTOut;
     coarseMean(2,:,:,im_num)=squeeze(coarseMean(2,:,:,im_num))+vel.u/nDTOut;
@@ -148,15 +153,15 @@ while im_num<nSnapshots
     coarseMean(5,:,:,im_num)=squeeze(coarseMean(5,:,:,im_num))+accel.dv/nDTOut;
     
     Area=cat(1,Floe.area);
-    dissolvedNEW = calc_vol_dissolved(Floe(Area<3e5),Nx,Ny,c2_boundary);
+    dissolvedNEW = calc_vol_dissolved(Floe(Area<3e5),Nx,Ny,c2_boundary_poly);
     %Vd(:,:,im_num) = Vd(:,:,im_num)+Dissolved_Ice(Vd,coarseMean,im_num,dissolvedNEW,c2_boundary,dt)/nDTOut;
     Vdnew = Dissolved_Ice(Vd,coarseMean,im_num,dissolvedNEW,c2_boundary,dt);
     Vd(:,:,2) = Vd(:,:,1);
    % Vd(:,:,1) = Vdnew;
     Vd(:,:,1)= Vd(:,:,1)+dissolvedNEW;
     
-    Floe=Floe(Area> 1e6);
-    if sum(Area<1e6)>0, display(['num of small floes killed:' num2str(sum(Area<1e6))]); end
+    Floe=Floe(Area> 3e5);
+    if sum(Area<3e5)>0, display(['num of small floes killed:' num2str(sum(Area<3e5))]); end
     Time=Time+dt; i_step=i_step+1; %update time index
 
 
